4f2695757d955e85e1864ddf76d94cdc9751ecc8
zxc5356061 <zxc5356061@gmail.com>
2021-12-30 16:27:29 +0800

    edit shared layout

---
+++ NTUB.FileManager.Site/Controllers/DocsController.cs
@@ -0,0 +1,34 @@
+﻿using NTUB.FileManager.Site.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NTUB.FileManager.Site.Controllers
+{
+    public class DocsController : Controller
+    {
+        // GET: Doc
+
+        //Template: List
+        //Reference script libraries: Untag
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Template: Create
+        //Reference script libraries: Tag
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(DocCreateVM model)
+        {
+            return View(model);
+        }
+    }
+}
