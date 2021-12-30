73faf453e094b6a2324064bb491b6b963f6319d5
zxc5356061 <zxc5356061@gmail.com>
2021-12-30 11:18:56 +0800

    public interface IDocRepository

---
+++ NTUB.FileManager.Site/Models/Interfaces/IDocRepository.cs
@@ -0,0 +1,18 @@
+﻿using NTUB.FileManager.Site.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTUB.FileManager.Site.Models.Interfaces
+{
+    public interface IDocRepository
+    {
+        void Create(DocEntity docEntity);
+        void Update(DocEntity docEntity);
+        void Delete(int docId);
+        IEnumerable<DocEntity> Search(string title, string description);
+        DocEntity Load(int docId);
+    }
+}
