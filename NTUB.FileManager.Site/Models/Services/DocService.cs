2b9061ae5b5bcea7f3a70943d2face804c97ca2d
zxc5356061 <zxc5356061@gmail.com>
2021-12-30 14:54:03 +0800

    DocService

---
+++ NTUB.FileManager.Site/Models/Services/DocService.cs
@@ -0,0 +1,38 @@
+﻿using NTUB.FileManager.Site.Models.DTOs;
+using NTUB.FileManager.Site.Models.Infrastructures;
+using NTUB.FileManager.Site.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.FileManager.Site.Models.Services
+{
+    public class DocService
+    {
+        private readonly IDocRepository _reop;
+        public DocService()//給service用的
+        {
+            this._reop = new DocRepository();
+        }
+        public DocService(IDocRepository reop)//給單元測試用的
+        {
+            this._reop = reop;
+        }
+
+        public void Create(CreateDocRequest request) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(EditDocRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int docId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
