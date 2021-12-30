cb296f118216993b785f8d12d3131b6ea7dff165
zxc5356061 <zxc5356061@gmail.com>
2021-12-30 11:33:18 +0800

    public class DocRepository : IDocRepository

---
+++ NTUB.FileManager.Site/Models/Infrastructures/DocRepository.cs
@@ -0,0 +1,37 @@
+﻿using NTUB.FileManager.Site.Models.Entities;
+using NTUB.FileManager.Site.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.FileManager.Site.Models.Infrastructures
+{
+    public class DocRepository : IDocRepository
+    {
+        public void Create(DocEntity docEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int docId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DocEntity Load(int docId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DocEntity> Search(string docTitle, string docDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(DocEntity docEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
